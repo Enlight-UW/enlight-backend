4905fe67fbec6253db935a832a34ff2d098d3863
Alex Kersten <alex@dividebyxero.com>
2012-11-15 22:50:27 -0600

    NetBeans skeleton files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Alex
+ *
+ * Created on November 15, 2012, 10:47 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
